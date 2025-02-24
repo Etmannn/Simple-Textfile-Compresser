9c720a67cbb1d2a8ea75a7b990f19ed6080de806
Ethan Cleminson <128926337+Etmannn@users.noreply.github.com>
2025-02-24 22:29:48 +0000

    Removed unnecessary byte encoding and decoding steps

--- simpletc.py
+++ simpletc.py
@@ -1,94 +1,91 @@
 # this function compresses a given text file
-def compress(file: str, encoding: str) -> str:
-  # opens the file and store the raw binary data
-  with open(file, 'rb') as f:
-    raw_data: bytes = f.read()
+def compress(file: str) -> str:
+    # opens the file and stores data as a string
+    with open(file, 'r') as f:
+        data: str = f.read()
 
-  # decodes the raw binary data into a string
-  data: str = raw_data.decode(encoding)
+    compressed_data: list[tuple] = []
+    multiplier: int = 1
 
-  compressed_data: list[tuple] = []
-  multiplier: int = 1
+    # groups characters together into tuples which are then appended into a list
+    for n in range(len(data) - 1):
+        # checks if the next character is equal to the current one
+        if data[n] == data[n + 1]:
+            # adds one to the multiplier
+            multiplier += 1
 
-  # groups characters together into tuples which are then appended into a list
-  for n in range(len(data)-1):
-    # checks if the next character is equal to the current one
-    if data[n] == data[n+1]:
-      # adds one to the multiplier
-      multiplier += 1
+        # checks if the next character is not equal to the current one
+        elif data[n] != data[n + 1]:
+            # appends tuple to the list and resets multiplier back to one
+            compressed_data.append(tuple([data[n], multiplier]))
+            multiplier = 1
 
-    # checks if the next character is not equal to the current one
-    elif data[n] != data[n+1]:
-      # appends tuple to the list and resets multiplier back to one
-      compressed_data.append(tuple([data[n], multiplier]))
-      multiplier = 1
+    # appends the final character
+    compressed_data.append(tuple([data[-1], multiplier]))
 
-  # appends the final character
-  compressed_data.append(tuple([data[-1], multiplier]))
+    # splits the file's name and suffix
+    substrings = file.split('.')
+    # creates new file name based on original file's name
+    newfile = f'compressed_{substrings[0]}_{substrings[1]}'
 
-  # splits the file's name and suffix
-  substrings = file.split('.')
-  # creates new file name based on original file's name
-  newfile = f'compressed_{substrings[0]}_{substrings[1]}'
+    # writes the new file
+    with open(newfile, 'w') as f:
+        f.write(str(compressed_data))
 
-  # writes the new file
-  with open(newfile, 'w') as f:
-    f.write(str(compressed_data))
-
-  # returns the name of the new file
-  return newfile
+    # returns the name of the new file
+    return newfile
 
 
-# this function extracts the data from a previously text file previously compressed by this program
+# this function extracts the data from a previous text file compressed by this program
 def extract(file: str, encoding: str) -> str:
-  # opens the compressed file
-  with open(file, 'r') as f:
-    compressed_data = eval(f.read())
-    
-  data:str = ''
+    # opens the compressed file
+    with open(file, 'r') as f:
+        compressed_data = eval(f.read())
 
-  # appends characters to a string
-  for character,multiplier in compressed_data:
-    # appends a character a specified amount to a string
-    for _ in range(multiplier):
-      data += character
+    data: str = ''
 
-  # encodes the string into raw binary data
-  raw_data = data.encode(encoding)
-  # splits the file's name and suffix
-  substrings = file.split('_')
-  # creates new file name based on original file's name
-  newfile = f'uncompressed_{substrings[1]}.{substrings[2]}'
+    # appends characters to a string
+    for character, multiplier in compressed_data:
+        # appends a character a specified amount to a string
+        for _ in range(multiplier):
+            data += character
 
-  # writes the new file
-  with open(newfile, 'wb') as f:
-    f.write(raw_data)
+    # splits the file's name and suffix
+    substrings = file.split('_')
+    # creates new file name based on original file's name
+    newfile = f'uncompressed_{substrings[1]}.{substrings[2]}'
 
-  # returns the name of the new file
-  return newfile
+    # writes the new file
+    with open(newfile, 'wb') as f:
+        f.write(data)
+
+    # returns the name of the new file
+    return newfile
+
 
 def main():
-  select = int(input('compress(1), extract(2), exit(3): '))
-    
-  try:	
-    if select == 1:
-      file = input('input filename: ')
-      newfile = compress(file=file, encoding='utf-8')
-      print(f'file successfully compressed as {newfile}\n')
+    select = int(input('compress(1), extract(2), exit(3): '))
 
-    elif select == 2:
-      file = input('input filename: ')
-      newfile = extract(file=file, encoding='utf-8')
-      print(f'file successfully extracted as {newfile}\n')
+    try:
+        if select == 1:
+            file = input('input filename: ')
+            newfile = compress(file=file)
+            print(f'file successfully compressed as {newfile}\n')
 
-    elif select == 3:
-      exit()
+        elif select == 2:
+            file = input('input filename: ')
+            newfile = extract(file=file)
+            print(f'file successfully extracted as {newfile}\n')
 
-    else:
-      print('invalid input\n')
-    
-  except Exception as e:
-    print(e)
+        elif select == 3:
+            exit()
+
+        else:
+            print('invalid input\n')
+
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
-  main()
+    main()
